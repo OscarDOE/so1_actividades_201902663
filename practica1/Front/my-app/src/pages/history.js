875335ce64f8672d9f292c799343a3cf33da95bc
OscarDOE <74039206+OscarDOE@users.noreply.github.com>
2023-02-19 23:33:43 -0600

    La aplicacion

--- practica1/Front/my-app/src/pages/history.js
+++ practica1/Front/my-app/src/pages/history.js
@@ -2,6 +2,9 @@ import { useEffect, useState } from 'react';
 import { useNavigate } from 'react-router';
 import '../App.css'
 
+import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap/dist/js/bootstrap.js';
+
 export const History = () =>{
     const [operations,setOperations] = useState([]);
 
@@ -10,26 +13,37 @@ export const History = () =>{
         navigate('/calculate')
     };
 
+    useEffect(()=>{
+        fetch('http://localhost:5005/history')
+        .then(res => res.json())
+        .then(res =>setOperations(res))
+            .catch(err => console.log(err))
+    },[])
+
 
     return (
         <div>
             <h1>HISTORIAL</h1>
-            <div className='grid-history'>
-                <div className='header-history'>Número 1</div>
-                <div className='header-history'>Operación</div>
-                <div className='header-history'>Número 2</div>
-                <div className='header-history'>Resultado</div>
-                <div className='header-history'>Fecha</div>
-                <div>1</div>
-                <div>1</div>
-                <div>1</div>
-                <div>1</div>
-                <div>1</div>
-                <div>1</div>
-                <div>1</div>
-                <div>1</div>
-                <div>1</div>
-                <div>1</div>
+            <div>
+                <div className='grid-history'>
+                    <div className='header-history'>Número 1</div>
+                    <div className='header-history'>Operación</div>
+                    <div className='header-history'>Número 2</div>
+                    <div className='header-history'>Resultado</div>
+                    <div className='header-history'>Fecha</div>
+                </div>
+                {operations.map(function(operacion,index){
+                    return(
+                        <div className='grid-history'>
+                            <div>{operacion.num1}</div>
+                            <div>{operacion.operation}</div>
+                            <div>{operacion.num2}</div>
+                            <div>{operacion.resultado}</div>
+                            <div>{operacion.fecha}</div>
+                        </div>
+                    )
+                })}
+
             </div>
 
             <button onClick={calculadora} className='btn btn-primary'>Calculadora</button>
---
+++ practica1/bash/script.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+# Cantidad de filas sin contar el encabezado
+total_filas=$(cat ../back/history.csv | wc -l)
+cantidad_filas=$((total_filas - 1))
+
+echo "Cantidad de filas: $cantidad_filas"
+
+# Cantidad de operaciones totales con error
+cantidad_error=$(awk -F, '{if ($4 == "-123457000.00") {print $0}}' ../back/history.csv | wc -l)
+
+echo "Cantidad de operaciones con error: $cantidad_error"
+
+# Cantidad de operaciones por separado
+cantidad_suma=$(awk -F, '{if ($2 == "+" && $4 != "-12345700.00") {print $0}}' ../back/history.csv | wc -l)
+cantidad_resta=$(awk -F, '{if ($2 == "-" && $4 != "-12345700.00") {print $0}}' ../back/history.csv | wc -l)
+cantidad_multiplicacion=$(awk -F, '{if ($2 == "*" && $4 != "-12345700.00") {print $0}}' ../back/history.csv | wc -l)
+cantidad_division=$(awk -F, '{if ($2 == "/" && $4 != "-12345700.00") {print $0}}' ../back/history.csv | wc -l)
+
+echo "Cantidad de operaciones de suma: $cantidad_suma"
+echo "Cantidad de operaciones de resta: $cantidad_resta"
+echo "Cantidad de operaciones de multiplicacion: $cantidad_multiplicacion"
+echo "Cantidad de operaciones de division: $cantidad_division"
+
+# Mostrar los datos del dÃ­a de hoy
+fecha=$(date +%Y-%m-%d)
+
+awk -v fecha="$fecha" -F, '{if ($5 ~ fecha) {print $0}}' ../back/history.csv
