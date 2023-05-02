62a836d411dee9ffe28041b08b05ed73b2fb3a52
Oscar Oliva <2905334300101@ingenieria.usac.edu.gt>
2023-05-01 18:42:32 -0600

    Update Script-1.sql

--- practica1/BD/Scripts/Script-1.sql
+++ practica1/BD/Scripts/Script-1.sql
@@ -13,3 +13,25 @@ SELECT * FROM games;
 
 
 drop table games ;
+show databases;
+
+
+-- Base de Datos 
+CREATE database prueba55;
+-- DROP database sounoproy2;
+
+CREATE TABLE votes(
+	id int not null primary key auto_increment,
+	sede int not null,
+	municipio varchar(50) not null,
+	departamento varchar(50) not null,
+	papeleta varchar(50) not null,
+	partido varchar(50) not null
+);
+
+INSERT INTO votes(sede, departamento, municipio, papeleta, partido ) 
+VALUES(1,'Guatemala','San Miguel Petapa','Rosada','VAMOS');
+
+
+
+
